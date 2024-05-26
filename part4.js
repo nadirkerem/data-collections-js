c85b3370557dd523095bb093b1b025c78b197251
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-05-26 00:03:57 -0400

    add explanations to each part

--- part4.js
+++ part4.js
@@ -1,3 +1,9 @@
+/*
+Part 4: Sorting and Manipulating Data
+It is important to know how to work with data in this format, an array of objects, as it is one of the most commonly used data formats in JavaScript.
+Using array methods, accomplish the following tasks, in order upon the result of Part 3:
+*/
+
 const csv1 =
   'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
 
@@ -40,7 +46,8 @@ newArray = newArray.filter(
 
 console.log(newArray);
 
-// 2
+// 2. Insert the following object at index 1:
+// { id: "48", name: "Barry", occupation: "Runner", age: "25" }
 
 newArray.splice(1, 0, {
   id: '48',
@@ -51,7 +58,8 @@ newArray.splice(1, 0, {
 
 console.log(newArray);
 
-// 3
+// 3. Add the following object to the end of the array:
+// { id: "7", name: "Bilbo", occupation: "None", age: "111" }
 
 // newArray.splice(newArray.length + 1, 0, {
 //   id: '7',
@@ -69,7 +77,7 @@ newArray.push({
 
 console.log(newArray);
 
-// 4
+// 4. Finally, use the values of each object within the array and the array’s length property to calculate the average age of the group. This calculation should be accomplished using a loop.
 
 let totalAge = 0;
 
